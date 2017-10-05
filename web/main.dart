ca3764b681e2a9c3ae13a1ada45d1c1a49f5b40e
Dennis Kaselow <dennis.kaselow+github@gmail.com>
2017-10-05 13:46:12 +0200

    bugfix; list of timers instead of a single timer

--- web/main.dart
+++ web/main.dart
@@ -3,13 +3,15 @@ import 'dart:math';
 import 'dart:html';
 
 final CanvasElement canvas = querySelector('canvas');
-Timer timer;
+List<Timer> timer = [];
 int sizeMulti;
 
 void main() {
   final ButtonElement button = querySelector('button');
   button.onClick.listen((_) {
-    timer?.cancel();
+    timer
+      ..forEach((t) => t.cancel())
+      ..clear();
     sizeMulti = 1;
     final input = (querySelector('textarea') as TextAreaElement).value;
     final cells = input
@@ -41,16 +43,14 @@ renderPixel(List<List<int>> cells, int color, int maxIterations,
   if (currentIteration > maxIterations) return;
   final size = sizeMulti * pow(3, maxIterations - currentIteration);
   canvas.context2D
-    ..beginPath()
     ..fillStyle = 'hsl(0, 0%, ${color * 100.0 / (cells[0][0]-1)}%)'
-    ..fillRect(posX * size, posY * size, size, size)
-    ..closePath();
-  timer = new Timer(const Duration(seconds: 1), () {
+    ..fillRect(posX * size, posY * size, size, size);
+  timer.add(new Timer(const Duration(seconds: 1), () {
     for (int x = 0; x < 3; x++) {
       for (int y = 0; y < 3; y++) {
         renderPixel(cells, cells[1 + color][y * 3 + x], maxIterations,
             currentIteration + 1, posX * 3 + x, posY * 3 + y);
       }
     }
-  });
+  }));
 }
